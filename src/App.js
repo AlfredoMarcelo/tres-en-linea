2279b6a6e090052277e7289312bce70c25e091c9
Alfredo Supanta Alave <alfredo.supanta@corebiz.ag>
2023-05-04 23:17:12 -0400

    feat: se agrega función de mostrar ganador

--- src/App.js
+++ src/App.js
@@ -5,33 +5,58 @@ function Square({value, onSquareClick}) {
 }
 
 export default function Board() {
+  const [xIsNext,setXIsNext] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null))
 
-  function handleClick(){
+  function handleClick(i){
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const newSquare = squares.slice()
-    newSquare[0] = 'X'
+    if(newSquare[i])return 
+    xIsNext? newSquare[i] = 'X': newSquare[i] = 'O'
     setSquares(newSquare)
-    console.log("se cambio a X");
+    setXIsNext(!xIsNext)
+    console.log(`se cambio el cuadro ${i + 1}`);
   }
 
   return (
     <Fragment>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick}/>
-        <Square value={squares[1]}/>
-        <Square value={squares[2]}/>
+        <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square value={squares[3]}/>
-        <Square value={squares[4]}/>
-        <Square value={squares[5]}/>
+        <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
       </div>
       <div className="board-row">
-        <Square value={squares[6]}/>
-        <Square value={squares[7]}/>
-        <Square value={squares[8]}/>
+        <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
       </div>
     </Fragment>
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
